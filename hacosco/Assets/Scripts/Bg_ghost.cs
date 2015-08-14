d09d449c16103fa3659b5a8143317fae95fbb547
hounenhounen <kawahara@kawaharafumishiki-no-MacBook-Air.local>
2015-08-14 17:20:01 +0900

    Commit

---
+++ hacosco/Assets/Scripts/Bg_ghost.cs
@@ -0,0 +1,42 @@
+﻿using NCMB; //mobile backendのSDKを読み込む
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using MiniJSON;
+
+public class Bg_ghost : MonoBehaviour {
+	public static NCMBObject posObj;
+	public static bool readyGhost = false;
+	// Use this for initialization
+	void Start () {
+		NCMBQuery<NCMBObject> query = new NCMBQuery<NCMBObject> ("Score");
+		query.OrderByDescending ("score");
+		query.Limit = 1;
+		query.FindAsync ((List<NCMBObject> objList ,NCMBException e) => {
+			
+			if (e != null) {
+				//検索失敗時の処理
+			} else {
+				//検索成功時の処理
+				
+				// 取得したレコードをscoreクラスとして保存
+				foreach (NCMBObject obj in objList) {
+					posObj = obj;
+					//--- 下記の２文は動かしてみて、問題なければ削除
+					//string s = System.Convert.ToString( ((ArrayList)((ArrayList)obj["Log"])[0])[1]);
+					//IList familyList = (IList)Json.Deserialize(System.Convert.ToString(obj));
+					
+				}
+				readyGhost = true;
+			}
+		});
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ hacosco/Assets/Scripts/Manager.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+	// Playerプレハブ
+	public GameObject player;
+	public GameObject ghost;
+	// タイトル
+	private GameObject title;
+
+	private bool leaderBoardButton;
+	private bool ghostButton;
+
+	 void Start ()
+	{
+		// Titleゲームオブジェクトを検索し取得する
+		title = GameObject.Find ("Title");
+	}
+	
+	void OnGUI ()
+	{
+		// ゲーム中ではなく、タッチまたはマウスクリック直後であればtrueを返す。
+		if (!IsPlaying()) {
+			drawButton();
+			// 画面タップでゲームスタート
+			if ( Event.current.type == EventType.MouseDown) 
+				GameStart (false);
+
+			// ランキングボタンが押されたら
+			if ( leaderBoardButton )
+				Application.LoadLevel("LeaderBoard");
+
+			//—--ゴーストボタンが押下場合の挙動定義---------
+			if ( ghostButton )
+				GameStart (true);
+			//----------------------------------------
+		}
+	}
+
+	void GameStart (bool withGhost)
+	{
+		// ゲームスタート時に、タイトルを非表示にしてプレイヤーを作成する
+		title.SetActive (false);
+
+		//------------------------------------------------------------------------------
+		if (withGhost == true) {
+			// ゴーストボタンを押下したらゴーストを表示する
+			Instantiate (ghost, ghost.transform.position, ghost.transform.rotation);
+			Instantiate (player, player.transform.position, player.transform.rotation);
+		} else {
+			// 画面を押下したらゴーストを表示しないでゲームを開始する
+			Instantiate (player, player.transform.position, player.transform.rotation);
+		}
+		//------------------------------------------------------------------------------
+	}
+	
+	public void GameOver ()
+	{
+		FindObjectOfType<Score> ().Save ();
+		Application.LoadLevel ("SaveScore");
+		// ゲームオーバー時に、タイトルを表示する
+		//title.SetActive (true);
+	}
+	
+	public bool IsPlaying ()
+	{
+		// ゲーム中かどうかはタイトルの表示/非表示で判断する
+		return title.activeSelf == false;
+	}
+
+	private void drawButton() {
+		// ボタンの設置
+		int btnW = 140, btnH = 50;
+		GUI.skin.button.fontSize = 18;
+		leaderBoardButton = GUI.Button( new Rect(0*btnW, 0, btnW, btnH), "Leader Board" );
+
+		//---Bg_ghost.csでゴーストデータを取得できたら、ゴーストボタンを表示する-------------
+		if (Bg_ghost.readyGhost == true) {
+				ghostButton = GUI.Button (new Rect (btnW, 0, btnW, btnH), "Ghost");
+		}
+		//--------------------------------------------------------------------------
+	}
+}
